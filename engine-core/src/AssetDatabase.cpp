f2ccd0b201a82b214d5e8c759b18513878856f66
Andras <andras.szabo.fin@gmail.com>
2026-08-31 20:25:17 +0200

    [WIP], asset database

--- engine-core/src/AssetDatabase.cpp
+++ engine-core/src/AssetDatabase.cpp
@@ -7,16 +7,49 @@ module AssetDatabase;
 
 #if defined ( __INTELLISENSE__ )
 #include "AssetDatabase.ixx"
+#include "DataFile.ixx"
 #else
+import DataFile;
 import std;
 #endif
 
 namespace Engine
 {
+	AssetDatabase::AssetDatabase()
+	{
+		_impl = new AssetDatabase_Impl{};
+	}
+
+	AssetDatabase::~AssetDatabase()
+	{
+		delete _impl;
+		_impl = nullptr;
+	}
+
+	AssetDatabase::AssetDatabase(AssetDatabase&& other)
+	{
+		if (other._impl != _impl)
+		{
+			_impl = other._impl;
+			other._impl = nullptr;
+		}
+	}
+
+	AssetDatabase& AssetDatabase::operator=(AssetDatabase&& other)
+	{
+		if (other._impl != _impl)
+		{
+			_impl = other._impl;
+			other._impl = nullptr;
+		}
+
+		return *this;
+	}
+
 	int AssetDatabase::GetAssetCount() const
 	{
-		assert(_pathsByGuid.size() == _guidsByPath.size() && "AssetDatabase internal size mismatch!");
-		return _pathsByGuid.size();
+		assert(_impl->pathsByGuid.size() == _impl->guidsByPath.size() && "AssetDatabase internal size mismatch!");
+		return static_cast<int>(_impl->pathsByGuid.size());
 	}
 
 	Engine::Expected<void> AssetDatabase::PopulateFromFolder(const std::filesystem::path& path)
@@ -54,12 +87,40 @@ namespace Engine
 					// TODO
 					//		- check if there's a corresponding .meta file, and create one if not
 					//		- then add it to the maps
+
+					// TODO what if there are assets called the same,
+					// but w/ different extensions :(
+
+					fs::path metaFilePath {};
+					Engine::GUID guid = Engine::GUID::Invalid();
+
+					if (!DoesMetaFileExist(entry, metaFilePath))
+					{
+						guid = CreateMetaFile(assetType, metaFilePath);
+					}
+					else
+					{
+						Engine::AssetType serializedAssetType{ Engine::AssetType::Undefined };
+						if (!TryExtractGuidAndAssetTypeFromMetaFile(metaFilePath, guid, serializedAssetType))
+						{
+							return Engine::Unexpected{ Engine::Error { Engine::ErrorType::File, "Guid collision!" } };
+						}
+
+						// TODO:
+						// if the serialized asset type is different to the actual one,
+						// maybe log a warning?
+					}
+
+					// So now we have a guid and an asset type;
+					// now to check that the guid is indeed unique in the map...
+
+					//TODO
 				}
 			}
 		}
 
-		_pathsByGuid = std::move(pathsByGuid);
-		_guidsByPath = std::move(guidsByPath);
+		_impl->pathsByGuid = std::move(pathsByGuid);
+		_impl->guidsByPath = std::move(guidsByPath);
 
 		return {};
 	}
@@ -70,6 +131,44 @@ namespace Engine
 		return true;
 	}
 
+	Engine::GUID AssetDatabase::CreateMetaFile(Engine::AssetType assetType,
+		const std::filesystem::path& metaFilePath) const
+	{
+		const Engine::GUID guid{};
+
+		Engine::DataFile out;
+
+		out["GUID"].SetULong(guid.id);
+		out["Type"].SetInt(static_cast<int>(assetType));
+		// TODO other things
+
+		Engine::DataFile::Serialize(out, metaFilePath.string());
+
+		return guid;
+	}
+
+	bool AssetDatabase::TryExtractGuidAndAssetTypeFromMetaFile(const std::filesystem::path& metaFilePath, 
+		Engine::GUID& guid, 
+		Engine::AssetType& type) const
+	{
+		const auto meta = Engine::DataFile::Deserialize(metaFilePath.string());
+		if (meta.has_value())
+		{
+			const auto& value = meta.value();
+
+			guid = value["GUID"].GetULong();
+			type = static_cast<Engine::AssetType>(value["Type"].GetInt());
+
+			return true;
+
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
 	AssetType AssetDatabase::IsAssetFile(const std::filesystem::directory_entry& directoryEntry) const
 	{
 		namespace fs = std::filesystem;
@@ -77,13 +176,26 @@ namespace Engine
 		if (directoryEntry.is_regular_file())
 		{
 			const std::wstring extensionAsString{ directoryEntry.path().extension() };
-			const auto typeByExtensionPair = _assetTypesByExtension.find(extensionAsString);
-			if (typeByExtensionPair != _assetTypesByExtension.end())
+			const auto typeByExtensionPair = _impl->assetTypesByExtension.find(extensionAsString);
+			if (typeByExtensionPair != _impl->assetTypesByExtension.end())
 			{
 				return (*typeByExtensionPair).second;
 			}
 		}
 
 		return AssetType::Undefined;
+	}
+
+	bool AssetDatabase::DoesMetaFileExist(const std::filesystem::directory_entry& directoryEntry,
+		std::filesystem::path& metaFilePath) const
+	{
+		namespace fs = std::filesystem;
+
+		const fs::path metaExtension{ ".meta" };
+
+		metaFilePath = directoryEntry.path();
+		metaFilePath.replace_extension(metaExtension);
+
+		return fs::exists(metaFilePath);
 	}
 } // namespace Engine
--- engine-core/src/AssetDatabase.ixx
+++ engine-core/src/AssetDatabase.ixx
@@ -21,15 +21,41 @@ import std;
 
 namespace Engine
 {
+	struct AssetDatabase_Impl
+	{
+		AssetDatabase_Impl()
+		{
+			for (const auto& [extension, type] : {
+					std::make_pair(std::wstring {L".scene"}, Engine::AssetType::Scene),		// TODO
+					std::make_pair(std::wstring {L".fbx"}, Engine::AssetType::StaticMesh),	// TODO
+					std::make_pair(std::wstring {L".png"}, Engine::AssetType::Texture),
+					// TODO
+				})
+			{
+				assetTypesByExtension.emplace(extension, Engine::AssetType::Undefined);
+			}
+		}
+
+		std::unordered_map<Engine::GUID, std::string> pathsByGuid;
+		std::unordered_map<std::string, Engine::GUID> guidsByPath;
+		std::unordered_map<std::wstring, Engine::AssetType> assetTypesByExtension;
+	};
+
 	export class ENGINE_CORE_API AssetDatabase
 	{
 	private:	
-		std::unordered_map<Engine::GUID, std::string> _pathsByGuid;
-		std::unordered_map<std::string, Engine::GUID> _guidsByPath;
-
-		std::unordered_map<std::wstring, Engine::AssetType> _assetTypesByExtension;
+		AssetDatabase_Impl* _impl { nullptr };
 
 	public:
+		AssetDatabase();
+		~AssetDatabase();
+
+		AssetDatabase(const AssetDatabase& other) = delete;
+		AssetDatabase& operator=(const AssetDatabase& other) = delete;
+
+		AssetDatabase(AssetDatabase&& other);
+		AssetDatabase& operator=(AssetDatabase&& other);
+
 		int GetAssetCount() const;
 
 		Engine::Expected<void> PopulateFromFolder(const std::filesystem::path& path);
@@ -37,6 +63,9 @@ namespace Engine
 
 	private:
 		bool DoesMatchFilter(const std::filesystem::directory_entry& directoryEntry) const;
+		bool DoesMetaFileExist(const std::filesystem::directory_entry& directoryEntry, std::filesystem::path& metaFilePath) const;
+		Engine::GUID CreateMetaFile(Engine::AssetType assetType, const std::filesystem::path& metaFilePath) const;
+		bool TryExtractGuidAndAssetTypeFromMetaFile(const std::filesystem::path& metaFilePath, Engine::GUID& guid, Engine::AssetType& type) const;
 	};
 
 } // namespace Engine
